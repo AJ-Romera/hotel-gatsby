7c390607b900baca550b491c29ba126d2b49bc20
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-04 11:54:32 +0200

    CGNH: Crea el Layout Principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import * as React from "react"
-/* import { StaticImage } from "gatsby-plugin-image" */
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
 
-const IndexPage = () => <h1>Hotel Gatsby</h1>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Hotel Gatsby</h1>
+    </Layout>
+  )
+}
 
 export default IndexPage
