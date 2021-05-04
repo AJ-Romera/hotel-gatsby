cd6e44b1590f7eb67cd22c00689c48b4ff08a3ec
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-04 16:02:32 +0200

    CHNG: Crea Styled Components de Navegación

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,45 @@
 import React from "react"
-
 import { Link } from "gatsby"
+import styled from "@emotion/styled"
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3rem;
+
+  @media (min-width: 768px) {
+    padding-bottom: 0;
+  }
+`
+
+const NavLink = styled(Link)`
+  color: #ffffff;
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 1rem;
+  font-family: "Grand Hotel", cursive;
+  text-decoration: none;
+  padding: 1rem;
+  margin-right: 1rem;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+  &.pagina-actual {
+    border-bottom: 2px solid #fff;
+  }
+`
 
 function Navegacion() {
   return (
-    <nav>
-      <Link to={"/"}>Inicio</Link>
-      <Link to={"/nosotros"}>Nosotros</Link>
-    </nav>
+    <Nav>
+      <NavLink to={"/"} activeClassName="pagina-actual">
+        Inicio
+      </NavLink>
+      <NavLink to={"/nosotros"} activeClassName="pagina-actual">
+        Nosotros
+      </NavLink>
+    </Nav>
   )
 }
 
