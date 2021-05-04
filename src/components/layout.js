2986e0afa289dc658024ec8ade179fb58cea304c
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-04 13:40:12 +0200

    CHNG: Añade CSS Global con Emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,43 @@
 import React from "react"
+
+import { Global, css } from "@emotion/react"
+
 import Header from "./header"
 import Helmet from "react-helmet"
 
 function Layout(props) {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+          }
+          body {
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+            font-family: "Grand Hotel", cursive;
+          }
+          h3 {
+            font-family: "Roboto", sans-serif;
+          }
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
       <Helmet>
         <title>Hotel Gatsby</title>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" />
