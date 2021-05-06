27e2da1df258a3fdf4f2e84e2095b6df8504b4af
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-06 13:06:24 +0200

    CHNG: Estiliza el Contenido de Sobre Nosotros

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -1,6 +1,25 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Image from "gatsby-image"
+import styled from "@emotion/styled"
+import { css } from "@emotion/react"
+
+const Contenido = styled.main`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 4rem;
+  }
+
+  p {
+    line-height: 2;
+  }
+`
 
 function ContenidoNosotros() {
   const resultado = useStaticQuery(graphql`
@@ -25,14 +44,22 @@ function ContenidoNosotros() {
 
   return (
     <>
-      <h2>{titulo}</h2>
-      <div>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >
+        {titulo}
+      </h2>
+      <Contenido>
         <p>{contenido}</p>
         <Image
           fluid={imagen.fluid}
           alt="Parte del hotel con vistas al mar, piscinas de lujo, paseos, zona chill-out"
         />
-      </div>
+      </Contenido>
     </>
   )
 }
