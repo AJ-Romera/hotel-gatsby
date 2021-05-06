b524008b16030e33e17539dc297e378e00a0981f
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-06 17:08:31 +0200

    CHNG: Hace el CSS de las Cards de Habitaciones en Inicio

--- src/components/habitacionPreview.js
+++ src/components/habitacionPreview.js
@@ -1,15 +1,48 @@
 import React from "react"
 import Image from "gatsby-image"
+import { css } from "@emotion/react"
+import styled from "@emotion/styled"
+import { Link } from "gatsby"
+
+const Boton = styled(Link)`
+  margin-top: 2rem;
+  padding: 1rem;
+  background-color: rgba(44, 62, 80, 0.85);
+  width: 100%;
+  color: #fff;
+  display: block;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: 700;
+  text-align: center;
+`
 
 function HabitacionPreview({ habitacion }) {
   const { titulo, contenido, imagen, slug } = habitacion
 
   return (
-    <div>
+    <div
+      css={css`
+        border: 1px solid #e1e1e1;
+        margin-bottom: 2rem;
+      `}
+    >
       <Image fluid={imagen.fluid} alt="Habitación" />
-      <div>
-        <h3>{titulo}</h3>
+      <div
+        css={css`
+          padding: 3rem;
+        `}
+      >
+        <h3
+          css={css`
+            font-size: 3rem;
+          `}
+        >
+          {titulo}
+        </h3>
         <p>{contenido}</p>
+
+        <Boton to={slug}>Ver Habitación</Boton>
       </div>
     </div>
   )
