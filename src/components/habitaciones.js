6518a2248b03ef290afa406cfd2da18d7275a1f4
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-07 17:48:23 +0200

    CHNG: Consulta una Habitación por Página Dinámica

--- src/components/habitaciones.js
+++ src/components/habitaciones.js
@@ -1,6 +1,25 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-function HabitacionTemplate() {
+export const query = graphql`
+  query($slug: String!) {
+    allDatoCmsHabitacion(filter: { slug: { eq: $slug } }) {
+      nodes {
+        titulo
+        contenido
+        imagen {
+          fluid(maxWidth: 1200) {
+            ...GatsbyDatoCmsFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+function HabitacionTemplate({ data }) {
+  console.log(data)
+
   return <h1>Desde habitaciones.js</h1>
 }
 
