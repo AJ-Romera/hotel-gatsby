e210530f787b30c1819e05041d993b5574d1c8bb
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-07 18:19:40 +0200

    CHNG: Hace el CSS de las Habitaciones y el Logo-Link del Header

--- src/components/habitaciones.js
+++ src/components/habitaciones.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Image from "gatsby-image"
+import { css } from "@emotion/react"
 import Layout from "../components/layout"
 
 export const query = graphql`
@@ -28,8 +29,21 @@ function HabitacionTemplate({
 
   return (
     <Layout>
-      <main>
-        <h1>{titulo}</h1>
+      <main
+        css={css`
+          margin: 0 auto;
+          max-width: 1200px;
+          width: 95%;
+        `}
+      >
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 4rem;
+          `}
+        >
+          {titulo}
+        </h1>
         <p>{contenido}</p>
         <Image fluid={imagen.fluid} alt="Foto de la habitación" />
       </main>
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,14 @@
 import React from "react"
-
 import { css } from "@emotion/react"
-
+import styled from "@emotion/styled"
+import { Link } from "gatsby"
 import Navegacion from "./nav"
+
+const EnlaceHome = styled(Link)`
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+`
 
 function Header() {
   return (
@@ -24,14 +30,9 @@ function Header() {
           }
         `}
       >
-        <h1
-          css={css`
-            color: #fff;
-            text-align: center;
-          `}
-        >
-          Hotel Gatsby
-        </h1>
+        <EnlaceHome to="/">
+          <h1>Hotel Gatsby</h1>
+        </EnlaceHome>
 
         <Navegacion />
       </div>
